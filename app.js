e079d7a2126bd27320273aea1710aa9afb79e668
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-12-23 23:06:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,68 @@
+// Imports
+const http = require('http')
+var fs = require('fs');
+var url = require('url');
+
+/* Register server: */
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    var q = url.parse(req.url, true);
+    var path = q.pathname;
+    console.log("Serving " + req.url);
+    res.write(req.url);
+
+    if(path == "/") {
+    	mainPage(res);
+    }
+    else if (path == "/append") {
+        appendData(res, q.query);
+    }
+    else if (path == "/character") {
+        charPage(res);
+    }
+    
+    res.end();
+
+    function mainPage(res) {
+        let mainHTML = fs.readFileSync('index.html', 'utf8');
+        res.write(mainHTML);
+        res.end();
+    
+    }
+    function charPage(res) {
+        // let header = fs.readFileSync('printlist.html', 'utf8');
+        let data = fs.readFileSync('charlist.lis', 'utf8');
+        // let word = [];
+        data = data.slice(0, -2);
+        lines = data.toString().split(",");
+    
+        if (lines == "" || lines == null){
+            let newform = fs.readFileSync('newcharform.html', 'utf8');
+            res.write(newform);
+        }
+        // else  {
+        //     let header = fs.readFileSync('printlist.html', 'utf8');
+        //     res.write("<table>")
+        //     res.write("<tr><th>Character Name</th><th>Character Class</th></tr>")
+            
+        //     for (let i = 0; i < lines.length; i++) {
+        //         let word = lines[i].split(" ");
+                
+        //         res.write("<td>"+word[0]+"</td>");
+        //         res.write("<td>"+word[1]+"</td>");
+        //         res.write("</tr>");
+        //         console.log(word[0], word[1])
+        //     };
+        //     res.write("</table>")
+        //     res.write(header);
+    
+        // };
+        // res.write(header);
+        // res.end();
+    }
+
+
+
+    
+}).listen(8080);
+
