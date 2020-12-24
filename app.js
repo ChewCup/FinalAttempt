a8e7236b141e7408526c820678be15ff610b3b2a
ChewCup <72440370+ChewCup@users.noreply.github.com>
2020-12-24 17:29:29 +0100

    print table done

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ http.createServer(function (req, res) {
     if(path == "/") {
     	mainPage(res);
     }
+    else if (path == "/main.css") {
+        loadmaincss(res);
+    }
     else if (path == "/append") {
         appendData(res, q.query);
     }
     else if (path == "/character") {
         charPage(res);
     }
-    
     res.end();
 
     function mainPage(res) {
@@ -29,8 +31,28 @@ http.createServer(function (req, res) {
         res.end();
     
     }
+
+    function loadmaincss(res) {
+        let mainCSS = fs.readFileSync('main.css', 'utf8');
+        res.write(mainCSS);
+        res.end();
+    }
+
+    function appendData(res, query) {
+        let header = fs.readFileSync('appendDataList.html', 'utf8');
+        var charname = query.charname;
+        var nameclass = query.nameclass;
+        
+        res.write("<table>")
+        res.write("<p>\Your character name is "+charname+" and your class is "+ nameclass+".\</p>")
+        res.write("</table>")
+        fs.appendFileSync('charlist.lis',charname+" "+nameclass+",\n")
+        res.write(header);
+        res.end();
+    }
+
     function charPage(res) {
-        // let header = fs.readFileSync('printlist.html', 'utf8');
+        let header = fs.readFileSync('printlist.html', 'utf8');
         let data = fs.readFileSync('charlist.lis', 'utf8');
         // let word = [];
         data = data.slice(0, -2);
@@ -40,25 +62,24 @@ http.createServer(function (req, res) {
             let newform = fs.readFileSync('newcharform.html', 'utf8');
             res.write(newform);
         }
-        // else  {
-        //     let header = fs.readFileSync('printlist.html', 'utf8');
-        //     res.write("<table>")
-        //     res.write("<tr><th>Character Name</th><th>Character Class</th></tr>")
+        else  {
+            res.write("<table>")
+            res.write("<tr><th>Character Name</th><th>Character Class</th></tr>")
             
-        //     for (let i = 0; i < lines.length; i++) {
-        //         let word = lines[i].split(" ");
+            for (let i = 0; i < lines.length; i++) {
+                let word = lines[i].split(" ");
                 
-        //         res.write("<td>"+word[0]+"</td>");
-        //         res.write("<td>"+word[1]+"</td>");
-        //         res.write("</tr>");
-        //         console.log(word[0], word[1])
-        //     };
-        //     res.write("</table>")
-        //     res.write(header);
+                res.write("<td>"+word[0]+"</td>");
+                res.write("<td>"+word[1]+"</td>");
+                res.write("</tr>");
+                console.log(word[0], word[1])
+            };
+            res.write("</table>")
+            res.write(header);
     
-        // };
-        // res.write(header);
-        // res.end();
+        };
+        res.write(header);
+        res.end();
     }
 
 
