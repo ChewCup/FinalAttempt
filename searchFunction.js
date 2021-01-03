fefd3739ede1d9943347e91cdf59eacfc4115ab4
ChewCup <72440370+ChewCup@users.noreply.github.com>
2021-01-03 18:31:20 +0100

    done

---
+++ searchFunction.js
@@ -0,0 +1,32 @@
+
+
+// var names = [];
+
+// let names = fs.readFileSync('charlist.lis', 'utf8');
+
+// for (let i = 0; i < names.length; i++) {
+//     let word = names[i].split(" ");
+//     console.log(word[0])
+// };
+
+// const seachInput = document.getElementById("mySearchInput");
+// seachInput.addEventListener("input", () => {
+//     console.log("input event fired");
+    
+// })
+
+
+
+// var input = document.getElementById("mySearchInput");
+// var button= document.getElementById("sb");
+
+// function searchfunction() {
+//     if (names.indexOf(number(input.value)) == -1) {
+//         document.write(input.value+" <b>Not found</b>");
+//     }
+//     else {
+//         document.write("")
+//     }
+// }
+
+// button.addEventListener("click", searchfunction)
